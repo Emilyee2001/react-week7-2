0013063e618dc3f7ea784fd552cac3a5fe6cb056
emilyt <emilyz_2001@hotmail.com>
2025-03-09 14:34:20 +0000

    批改修正

--- src/components/admin/Pagination.jsx
+++ src/components/admin/Pagination.jsx
@@ -9,19 +9,19 @@ function Pagination({pageInfo, getProductData}) {
       <nav>
         <ul className="pagination">
           <li className="page-item">
-            <a onClick={() => { handlePageChange(pageInfo.current_page - 1) }} className={`page-link ${!pageInfo.has_pre && 'disabled'}`} style={{ cursor: 'pointer' }}>
+            <a onClick={(e) => { e.preventDefault(); handlePageChange(pageInfo.current_page - 1) }} className={`page-link ${!pageInfo.has_pre && 'disabled'}`} style={{ cursor: 'pointer' }}>
               上一頁
             </a>
           </li>
           {[...Array(pageInfo.total_pages).keys()].map(page => (
             <li key={page + 1} className="page-item">
-              <a onClick={() => { handlePageChange(page + 1) }} className={`page-link ${pageInfo.current_page == page + 1 && 'active'}`} style={{ cursor: pageInfo.current_page == page + 1 ? 'not-allowed' : 'pointer' }}>
+              <a onClick={(e) => { e.preventDefault(); handlePageChange(page + 1) }} className={`page-link ${pageInfo.current_page == page + 1 && 'active'}`} style={{ cursor: pageInfo.current_page == page + 1 ? 'not-allowed' : 'pointer' }}>
                 {page + 1}
               </a>
             </li>
           ))}
           <li className="page-item">
-            <a onClick={() => { handlePageChange(pageInfo.current_page + 1) }} className={`page-link ${!pageInfo.has_next && 'disabled'}`} style={{ cursor: 'pointer' }}>
+            <a onClick={(e) => { e.preventDefault(); handlePageChange(pageInfo.current_page + 1) }} className={`page-link ${!pageInfo.has_next && 'disabled'}`} style={{ cursor: 'pointer' }}>
               下一頁
             </a>
           </li>
